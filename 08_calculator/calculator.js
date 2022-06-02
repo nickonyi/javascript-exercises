988c166c42d73a578734f8969a46abe60b38499e
nickonyi <nickoluoch1130@gmail.com>
2022-06-02 06:21:11 +0300

    Calculator Testing

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,51 @@
-const add = function() {
-	
+const objs = [];
+
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(objs) {
+    const sumTotal = objs.reduce((total, obj) => {
+        return total + obj;
+    }, 0);
+
+    return sumTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(objs) {
+    const multiple = objs.reduce((prevValue, currentValue) => {
+        return prevValue * currentValue;
+    }, 1);
+    return multiple;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+    let answer = 1;
+    if (n === 0 || n === 1) {
+        return answer;
+    } else {
+        for (i = n; i >= 1; i--) {
+            answer *= i;
+        }
+        return answer;
+    }
 };
 
-// Do not edit below this line
+
+//Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
