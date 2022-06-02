d47f3f21b1710c92ebeea74c77886a05ead66059
nickonyi <nickoluoch1130@gmail.com>
2022-06-02 09:29:49 +0300

    Calculator Testing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    const re = /[^A-Za-z0-9]/g;
 
+    let lowRegStr = string.toLowerCase().replace(re, "");
+    let reverseStr = lowRegStr.split('').reverse().join('');
+
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
