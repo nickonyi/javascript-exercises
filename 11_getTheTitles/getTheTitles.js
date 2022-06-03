7698e1554e00d8f1b8197b99a4b642d23966541f
nickonyi <nickoluoch1130@gmail.com>
2022-06-03 09:45:27 +0300

    get the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
+const books = [{
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
 
+const getTheTitles = function(books) {
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
